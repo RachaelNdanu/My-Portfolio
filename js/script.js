a2a11df69d0f8b1f4c1e0b8fee8ce0434285ee4f
Rachael <rndanu102@gmail.com>
2024-10-02 15:05:33 +0300

    adding automation

--- js/script.js
+++ js/script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Cybersecurity analyst", "Web-developer", "Cloud engineer"],
+    strings: ["Cybersecurity analyst", "Web-developer", "Automation engineer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Cybersecurity analyst", "Web-developer", "Cloud engineer"],
+    strings: ["Cybersecurity analyst", "Web-developer", "Automation engineer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
